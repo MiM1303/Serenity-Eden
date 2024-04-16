f7232d25640defd2a96375f632143ea9b044a0e8
Jannatul Ferdous Mirza <jfmirza1303@gmail.com>
2024-04-16 18:12:38 +0600

    added error page

--- src/pages/Error/ErrorPage.jsx
+++ src/pages/Error/ErrorPage.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 
-
 const ErrorPage = () => {
     return (
-        <div className="text-center">
+        <div className="text-center mt-48">
             <h2 className="text-red-600 text-5xl">Page not found!!!</h2>
             <p className="text-3xl">This page does not exist.</p>
-            <p className="text-2xl">Go back to <Link className="text-blue-600" to={-1}>previous page</Link> </p>
+            <p className="text-2xl">Go back to 
+                <Link className="text-blue-600 font-semibold" to={-1}> previous page</Link> 
+            </p>
         </div>
     );
 };
